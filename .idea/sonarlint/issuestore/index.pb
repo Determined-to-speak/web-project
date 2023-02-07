9f05f6cf53c526f44e658f1036ffdb59d629d460
lizhijiang <lizhijiang@fang.com>
2023-02-07 17:01:02 +0800

    添加network-main模块

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+B
+shiro-main/HELP.md,8\c\8cca4d9f5e86845f44ed89d2c79c21e8681366a3
---
+++ network-main/src/main/java/com/example/networkmain/NetworkMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.networkmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkMainApplication.class, args);
+    }
+
+}
