ba68be15e23a5eacdf16f2812d8ef774b391b478
lizhijiang <lizhijiang@fang.com>
2023-02-07 16:30:36 +0800

    Initial commit

---
+++ shiro-main/src/main/java/com/example/shiromain/ShiroMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.shiromain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroMainApplication.class, args);
+    }
+
+}
