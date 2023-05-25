19fa58907ed27063474e8cf7939ec049422ec8e6
立志讲讲 <1339049133@qq.com>
2023-05-25 23:24:39 +0800

    5.24 offer 9 10 11 12

---
+++ javabasis-main/src/main/java/com/example/javabasismain/swordfingeroffer/Offer11.java
@@ -0,0 +1,49 @@
+package com.example.javabasismain.swordfingeroffer;
+
+/**
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ * <p>
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/qing-wa-tiao-tai-jie-wen-ti-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Offer11 {
+
+    public static void main(String[] args) {
+        System.out.println(numWays(2));
+        System.out.println(numWays(7));
+        System.out.println(numWays(0));
+
+    }
+
+
+    /**
+     * 这道题其实也是斐波那契数列
+     *
+     * @param n
+     * @return
+     */
+
+
+    public static int numWays(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int a = 1;
+        int b = 2;
+        int result = -1;
+        for (int i = 3; i <= n; i++) {
+            result = a + b;
+            result %= 1000000007;
+            a = b;
+            b = result;
+        }
+        return result;
+    }
+
+}
---
+++ javabasis-main/src/main/java/com/example/javabasismain/swordfingeroffer/Offer12.java
@@ -0,0 +1,31 @@
+package com.example.javabasismain.swordfingeroffer;
+
+/**
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ * <p>
+ * 给你一个可能存在 重复 元素值的数组 numbers ，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。请返回旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为 1。  
+ * <p>
+ * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Offer12 {
+    public static void main(String[] args) {
+        System.out.println(minArray(new int[]{3, 4, 5, 1, 2}));
+        System.out.println(minArray(new int[]{2, 2, 2, 0, 1}));
+    }
+
+    public static int minArray(int[] numbers) {
+        if (numbers.length == 1) {
+            return numbers[0];
+        }
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i + 1] < numbers[i]) {
+                return numbers[i + 1];
+            }
+        }
+        return numbers[0];
+    }
+}
