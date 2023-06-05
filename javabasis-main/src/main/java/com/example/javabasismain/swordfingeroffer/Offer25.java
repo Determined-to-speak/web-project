1d33309bca01ed08f7ddcfa1021397dbf1d6eb7f
立志讲讲 <1339049133@qq.com>
2023-06-05 15:42:11 +0800

    6.4 offer 25,26

---
+++ javabasis-main/src/main/java/com/example/javabasismain/swordfingeroffer/Offer25.java
@@ -0,0 +1,44 @@
+package com.example.javabasismain.swordfingeroffer;
+
+import com.example.javabasismain.swordfingeroffer.util.ListNode;
+import com.example.javabasismain.swordfingeroffer.util.ListNodeUtil;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * <p>
+ * 输出：1->1->2->3->4->4
+ * <p>
+ * https://leetcode.cn/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/
+ */
+public class Offer25 {
+    public static void main(String[] args) {
+        int[] l1 = new int[]{1, 2, 4};
+        int[] l2 = new int[]{1, 3, 4};
+        System.out.println(Arrays.toString(ListNodeUtil.parseListNodeToArray(mergeTwoLists(ListNodeUtil.paseArrayToListNode(l1), ListNodeUtil.paseArrayToListNode(l2)))));
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode result = null;
+        if (l1.val < l2.val) {
+            result = l1;
+            l1 = l1.next;
+            result.next = mergeTwoLists(l1, l2);
+        } else {
+            result = l2;
+            l2 = l2.next;
+            result.next = mergeTwoLists(l1, l2);
+        }
+        return result;
+    }
+}
---
+++ javabasis-main/src/main/java/com/example/javabasismain/swordfingeroffer/Offer26.java
@@ -0,0 +1,35 @@
+package com.example.javabasismain.swordfingeroffer;
+
+import com.example.javabasismain.swordfingeroffer.util.TreeNode;
+
+/**
+ * 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+ * <p>
+ * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+ * <p>
+ * https://leetcode.cn/problems/shu-de-zi-jie-gou-lcof/
+ */
+public class Offer26 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (B == null || A == null) return false;
+
+        return EqualTree(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    public static boolean EqualTree(TreeNode a, TreeNode b) {
+        if (b == null) {
+            return true;
+        }
+        if (a == null) {
+            return false;
+        }
+        if (a.val != b.val) {
+            return false;
+        }
+        return a.val == b.val && EqualTree(a.left, b.left) && EqualTree(a.right, b.right);
+    }
+}
