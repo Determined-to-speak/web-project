ee2d2dc3fadfd3ea25e339ce976ed1011cb2c65e
立志讲讲 <1339049133@qq.com>
2023-06-19 18:51:10 +0800

    删除多余的文件

--- springboot-main/src/main/java/com/example/springbootmain/classLoad/ClassLoadTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootmain.classLoad;
-
-public class ClassLoadTest {
-    public static void main(String[] args) {
-        System.out.println(Thread.currentThread().getContextClassLoader());
-        System.out.println(Thread.currentThread().getName());
-        System.out.println(Thread.currentThread().getThreadGroup());
-    }
-}
