6bfdef2630aae94057329946195f76b992580174
立志讲讲 <1339049133@qq.com>
2023-07-13 11:42:52 +0800

    Offer 47

---
+++ javabasis-main/src/main/java/com/example/javabasismain/swordfingeroffer/Offer47.java
@@ -0,0 +1,40 @@
+package com.example.javabasismain.swordfingeroffer;
+
+
+/**
+ * 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/li-wu-de-zui-da-jie-zhi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Offer47 {
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        System.out.println(maxValue(grid));
+    }
+
+
+    public static int maxValue(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                } else if (i == 0 && j != 0) {
+                    grid[i][j] += grid[i][j - 1];
+                } else if (i != 0 && j == 0) {
+                    grid[i][j] += grid[i - 1][j];
+                } else if (i != 0 && j != 0) {
+                    grid[i][j] = grid[i][j] + Math.max(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+
+
+}
