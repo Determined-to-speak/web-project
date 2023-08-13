9b001f0685a0dc97cb2e11f8f6a43eff44e67461
立志讲讲 <1339049133@qq.com>
2023-08-13 22:05:53 +0800

    8.13学习笔记

---
+++ redis-main/src/main/java/com/example/redismain/RedisMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.redismain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisMainApplication.class, args);
+    }
+
+}
---
+++ redis-main/src/test/java/com/example/redismain/RedisMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redismain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
